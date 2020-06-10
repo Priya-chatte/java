e9cb8a991b930972a258b48c4647ca7e7f3e61ba
Priya-chatte <chatte.priya@gmail.com>
2020-06-10 15:35:12 +0530

    first commit

---
+++ java/src/program/tyme.java
@@ -0,0 +1,19 @@
+package program;
+
+public class tyme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=5;
+		int b=4;
+		int c;
+		c=a+b;
+		
+		
+		System.out.println(c);
+		
+      // System.out.println("structure name");
+	}
+
+}
