70dd60b01ed8a413a02f390f65c9a7c565dfe971
Priya-chatte <chatte.priya@gmail.com>
2020-06-10 16:05:45 +0530

    demo

---
+++ java/src/program/Demo.java
@@ -0,0 +1,28 @@
+package program;
+
+public class Demo 
+{
+
+    //static int a=10, b=5, c,d;
+
+public static void main(String[] args) 
+ {
+	
+	int a=10 ,b=5, c, d;
+	
+	c=a*b;
+	
+	d=a+b;
+	
+	System.out.println("value of c is" +c+"value of d is"+d);
+	
+	System.out.println("value of d is"+d);
+	
+	System.out.println("value of c is"+c);
+	
+	System.out.println(c);
+	
+	
+ }
+
+}
